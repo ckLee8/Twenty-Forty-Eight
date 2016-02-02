350e3772d8e5fd04abcbce91ed55bbebf793cdfa
Christopher Lee <zerosxl@gmail.com>
2016-02-02 17:13:23 -0500

    setup application controller

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,14 +1,7 @@
 'use strict';
 
-/**
- * @ngdoc overview
- * @name twentyfortyeightApp
- * @description
- * # twentyfortyeightApp
- *
- * Main module of the application.
- */
 angular
-  .module('twentyfortyeightApp', [
-    'ngCookies'
-  ]);
+  .module('twentyfortyeightApp', ['Game'])
+  .controller('GameController', function(GameManager) {
+  	this.game = GameManager;
+  });
