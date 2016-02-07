bb7709acd45d3ce4c94fa4ab6681d8ffb29600be
Christopher Lee <zerosxl@gmail.com>
2016-02-06 22:34:21 -0500

    add some board helper methods

--- app/scripts/board/board.js
+++ app/scripts/board/board.js
@@ -10,9 +10,9 @@ angular.module('Board', [])
 .service('BoardService', function(TileModel) {
 	this.buildEmptyGameBoard = function() {
       var self = this;
-      // Initialize our grid
+      // Initialize our board
       for (var x = 0; x < service.size * service.size; x++) {
-        this.grid[x] = null;
+        this.board[x] = null;
       }
 
       this.forEach(function(x,y) {
@@ -24,4 +24,37 @@ angular.module('Board', [])
 	this.tiles.push(new TileModel({x: 1, y: 1}, 2));
   	this.tiles.push(new TileModel({x: 1, y: 2}, 2));
 	this.size = 4;
+
+	this.forEach = function(cb) {
+		var totalSize = this.size * this.size;
+		for (var i = 0; i < totalSize; i++) {
+			var pos = this._positionToCoordinates(i);
+			cb(pos.x, pos.y, this.tiles[i]);
+		}
+	};
+	
+// Set a cell at position
+	this.setCellAt = function(pos, tile) {
+	if (this.withinGrid(pos)) {
+		var xPos = this._coordinatesToPosition(pos);
+		this.tiles[xPos] = tile;
+		}
+	};
+
+// Fetch a cell at a given position
+	this.getCellAt = function(pos) {
+		if (this.withinGrid(pos)) {
+			var x = this._coordinatesToPosition(pos);
+			return this.tiles[x];
+		} else {
+			return null;
+		}
+	};
+
+// A small helper function to determine if a position is
+// within the boundaries of our grid
+	this.withinGrid = function(cell) {
+		return cell.x >= 0 && cell.x < this.size &&
+		cell.y >= 0 && cell.y < this.size;
+	};
 });
