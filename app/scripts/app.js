eb7b8b184f0a160957cf1f15d66b76c66c647236
Christopher Lee <zerosxl@gmail.com>
2016-02-16 14:29:59 -0500

    add game initializer

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,7 +1,22 @@
 'use strict';
 
 angular
-.module('twentyfortyeightApp', ['Game'])
-.controller('GameController', function(GameManager) {
+.module('twentyfortyeightApp', ['Game', 'Controls'])
+.controller('GameController', function(GameManager, ControlsService) {
 	this.game = GameManager;
+
+	this.newGame = function() {
+    	ControlsService.init();
+    	this.game.newGame();
+    	this.startGame();
+  	};
+
+  	this.startGame = function() {
+    	var self = this;
+    	ControlsService.on(function(key) {
+      		self.game.move(key);
+    	});
+    };
+ 
+  	this.newGame();
 });
